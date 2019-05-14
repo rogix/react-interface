3a93182427784c314b4d0d2d026e33b5be07e5af
Roger <12768403+rogix@users.noreply.github.com>
2019-05-14 17:37:09 -0300

    seacrh logic

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,39 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: '',
       lastIndex: 0
     };
 
     this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+  }
+  
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    }); 
+  }
+  
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    });
   }
 
   deleteAppointment(apt) {
@@ -46,16 +75,47 @@ class App extends Component {
   }
 
   render() {
+
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if (this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts = filteredApts.sort((a, b) => {
+      if (a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase()) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    }).filter(eachItem => {
+      return(
+        eachItem['petName'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
+        eachItem['ownerName'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
+        eachItem['aptNotes'].toLowerCase().includes(this.state.queryText.toLowerCase())
+      );
+    });
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments formDisplay={this.state.formDisplay} />
-                <SearchAppointmenrs />
+                <AddAppointments 
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm} 
+                  addAppointment={this.addAppointment}
+                />
+                <SearchAppointmenrs 
+                  orderBy={this.state.orderBy}
+                  orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
+                />
                 <ListAppointments 
-                  appointments={this.state.myAppointments} 
+                  appointments={filteredApts} 
                   deleteAppointment={this.deleteAppointment} 
                 />
               </div>
