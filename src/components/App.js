01ec99ceb9c0c4035cf64ea2662d8f7f3fc946e1
Roger <12768403+rogix@users.noreply.github.com>
2019-05-15 16:10:58 -0300

    changing appointments

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import AddAppointments from './AddAppointments';
 import SearchAppointmenrs from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-import { without } from 'lodash';
+import { without, findIndex } from 'lodash';
 
 class App extends Component {
 
@@ -25,6 +25,7 @@ class App extends Component {
     this.addAppointment = this.addAppointment.bind(this);
     this.changeOrder = this.changeOrder.bind(this);
     this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
   }
   
   toggleForm() {
@@ -43,6 +44,17 @@ class App extends Component {
     this.setState({
       orderBy: order,
       orderDir: dir
+    });
+  }
+
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
     });
   }
 
@@ -125,6 +137,7 @@ class App extends Component {
                 <ListAppointments 
                   appointments={filteredApts} 
                   deleteAppointment={this.deleteAppointment} 
+                  updateInfo={this.updateInfo}
                 />
               </div>
             </div>
